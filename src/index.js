dcc88fffd4b54942e88504e168cd6ae921033b32
Ratheesh <ratheesh.v.2015.cse@rajalakshmi.edu.in>
2021-09-15 01:33:57 +0530

    ready for deploy

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './components/App'
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDom.render (
+    <App />,document.getElementById('root')
+)
